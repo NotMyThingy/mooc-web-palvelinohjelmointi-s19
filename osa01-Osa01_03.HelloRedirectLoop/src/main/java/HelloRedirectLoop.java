8aa67a5055e42bc5a2d87abc4fab72b6faec8e48
NotMyThingy <miska.janhunen@protonmail.com>
2019-10-31 14:32:30 +0200

    302 Found

--- osa01-Osa01_03.HelloRedirectLoop/src/main/java/HelloRedirectLoop.java
+++ osa01-Osa01_03.HelloRedirectLoop/src/main/java/HelloRedirectLoop.java
@@ -9,5 +9,30 @@ public class HelloRedirectLoop {
     public static void main(String[] args) throws Exception {
         ServerSocket server = new ServerSocket(8080);
 
+        while (true) {
+            // waiting for a request
+            Socket req = server.accept();
+
+            // read request
+            Scanner reader = new Scanner(req.getInputStream());
+
+            String message = reader.nextLine();
+            if (message.contains("/quit")) {
+                break;
+            }
+
+            // write response
+            PrintWriter writer = new PrintWriter(req.getOutputStream());
+            writer.println("HTTP/1.1 302 Found");
+            writer.println("Location: http://localhost:8080");
+            writer.println();
+            writer.flush();
+
+            // closing resources
+            writer.close();
+            reader.close();
+            req.close();
+        }
+
     }
 }

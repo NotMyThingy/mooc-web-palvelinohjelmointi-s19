0e56debfc1c07c02ea42e54fba567bc6a5bb8e6f
NotMyThingy <miska.janhunen@protonmail.com>
2019-11-15 13:04:43 +0200

    added @transactional, and refactored method for transfer

--- osa03-Osa03_04.BankTransfer/src/main/java/banktransfer/AccountController.java
+++ osa03-Osa03_04.BankTransfer/src/main/java/banktransfer/AccountController.java
@@ -1,7 +1,9 @@
 package banktransfer;
 
+import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,23 +15,48 @@ public class AccountController {
     @Autowired
     private AccountRepository accountRepository;
 
+    @PostConstruct
+
+    public void init() {
+
+        // test data to the application
+        Account account = new Account();
+        account.setIban("0000");
+        account.setBalance(1000);
+        accountRepository.save(account);
+
+        Account account2 = new Account();
+        account2.setIban("0001");
+        account2.setBalance(500);
+        accountRepository.save(account2);
+
+        Account account3 = new Account();
+        account3.setIban("0002");
+        account3.setBalance(50);
+        accountRepository.save(account3);
+    }
+
     @GetMapping("/")
     public String list(Model model) {
         model.addAttribute("accounts", this.accountRepository.findAll());
         return "index";
     }
 
+    @Transactional
     @PostMapping("/")
     public String transfer(@RequestParam String from, @RequestParam String to, @RequestParam Integer amount) {
-        Account accountFrom = this.accountRepository.findByIban(from);
-        Account accountTo = this.accountRepository.findByIban(to);
+        accountRepository
+                .findByIban(from)
+                .setBalance(accountRepository
+                        .findByIban(from)
+                        .getBalance() - amount);
 
-        accountFrom.setBalance(accountFrom.getBalance() - amount);
-        accountTo.setBalance(accountTo.getBalance() + amount);
+        accountRepository
+                .findByIban(to)
+                .setBalance(accountRepository
+                        .findByIban(to)
+                        .getBalance() + amount);
 
-        this.accountRepository.save(accountFrom);
-        this.accountRepository.save(accountTo);
-        
         return "redirect:/";
     }
 }

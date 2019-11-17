91afadbef5588e4cead06fd81d050517893faf80
NotMyThingy <miska.janhunen@protonmail.com>
2019-11-17 15:01:48 +0200

    Removed N+1 db request issue w/ @EntityGraph

--- osa03-Osa03_05.NamesAndAddresses/src/main/java/namesandaddresses/Person.java
+++ osa03-Osa03_05.NamesAndAddresses/src/main/java/namesandaddresses/Person.java
@@ -2,11 +2,16 @@ package namesandaddresses;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedAttributeNode;
+import javax.persistence.NamedEntityGraph;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
+@NamedEntityGraph(name = "Person.addresses",
+        attributeNodes = {
+            @NamedAttributeNode("address")})
 @Entity
 @Data
 @NoArgsConstructor

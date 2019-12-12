c7dee1c15b89410784f85a8ce89a05275b65ebcc
NotMyThingy <miska.janhunen@protonmail.com>
2019-12-12 13:09:35 +0200

    Validating input data

---
+++ osa06-Osa06_06.Registration/src/main/java/registration/Registration.java
@@ -0,0 +1,28 @@
+package registration;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Registration extends AbstractPersistable<Long> {
+
+    @Size(min = 4, max = 50)
+    private String name;
+
+    @Size(min = 4, max = 50)
+    private String address;
+
+    @NotEmpty
+    @Email
+    private String email;
+
+}

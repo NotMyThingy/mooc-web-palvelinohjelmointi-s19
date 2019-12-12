1977bc68944d0f10343c0b056cf37aa00c79be27
NotMyThingy <miska.janhunen@protonmail.com>
2019-12-12 13:13:29 +0200

    Super simple JavaScript calculator

---
+++ osa07-Osa07_01.Calculator/src/main/resources/public/javascript/calculator.js
@@ -0,0 +1,23 @@
+function haeNumero(tunnus) {
+    return parseInt(document.getElementById(tunnus).value);
+}
+
+function asetaTulos(tulos) {
+    document.getElementById("tulos").innerHTML = tulos;
+}
+
+function plus() {
+    asetaTulos(haeNumero("eka") + haeNumero("toka"));
+}
+
+function kerto() {
+    asetaTulos(haeNumero("eka") * haeNumero("toka"));
+}
+
+function miinus() {
+    asetaTulos(haeNumero("eka") - haeNumero("toka"));
+}
+
+function jako() {
+    asetaTulos(haeNumero("eka") / haeNumero("toka"));
+}
